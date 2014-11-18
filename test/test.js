01617d08d1109fa54886823f55f3a3f7ca8c76a2
Florian Traverse <ft@allocab.com>
2014-11-18 10:49:30 +0100

    fix to behave like the offical tester + more test

--- test/test.js
+++ test/test.js
@@ -5,6 +5,9 @@
 // Signature: chaRF2hTJKOScPr-RQCEhZbSzIE=
 // Full Signed URL: https://maps.googleapis.com/maps/api/geocode/json?address=New+York&client=clientID&signature=Qq5Gl6GE-7nkFMlDfx0TACxSKEw=
 
+
+//Note as we encode url, we have a slightly different result than the google example ( "+" becoming "%2B"). It has been validated against the official tester https://m4b-url-signer.appspot.com/
+
 var qs = require('..');
 var should = require('chai').should();
 
@@ -15,10 +18,10 @@ describe('When using an "options" object with a signature', function() {
   var query = {
     address: "New+York",
     client: "clientID",
-    signature: "chaRF2hTJKOScPr-RQCEhZbSzIE="
+    signature: "vNIXE0xscrmjlyV-12Nj_BvUPaw="
   };
 
-  var expectedQS = 'address=New+York&client=clientID&signature=Qq5Gl6GE-7nkFMlDfx0TACxSKEw=';
+  var expectedQS = 'address=New%2BYork&client=clientID&signature=WrTAou0zgXHjKwUPdsITPZeMXew=';
 
   describe('and a partial url', function() {
 
@@ -29,7 +32,7 @@ describe('When using an "options" object with a signature', function() {
 
       queryString.should.be.a('string');
 
-      decodeURIComponent(queryString).should.equal(expectedQS);
+      queryString.should.equal(expectedQS);
 
     });
   });
@@ -43,13 +46,45 @@ describe('When using an "options" object with a signature', function() {
 
       queryString.should.be.a('string');
 
-      decodeURIComponent(queryString).should.equal(expectedQS);
+      queryString.should.equal(expectedQS);
+
+
+    });
+  });
+
+  describe('with an url taken in real life', function() {
+
+    var url =
+      "https://maps.googleapis.com/maps/api/distancematrix/json?origins=45.779633%2C4.7951756%7C45.779507%2C4.7954993%7C45.767522962149876%2C4.875612258911133%7C45.77998%2C4.882435&destinations=45.779632568359375%2C4.795175552368164&mode=driving&units=metric&language=fr&avoid=&client=gme-foobar";
+
+    var query = {
+      origins: "45.779633,4.7951756|45.779507,4.7954993|45.767522962149876,4.875612258911133|45.77998,4.882435",
+      destinations: "45.779632568359375,4.795175552368164",
+      mode: "driving",
+      units: "metric",
+      language: "fr",
+      avoid: "",
+      client: "gme-foobar",
+      signature: "vNIXE0xscrmjlyV-12Nj_BvUPaw="
+    };
+
+    var expectedQS =
+      "origins=45.779633%2C4.7951756%7C45.779507%2C4.7954993%7C45.767522962149876%2C4.875612258911133%7C45.77998%2C4.882435&destinations=45.779632568359375%2C4.795175552368164&mode=driving&units=metric&language=fr&avoid=&client=gme-foobar&signature=m8HRPtb6lEfcBnYvEQrLmm-_AO4=";
+
+    it('it should match the verifier result', function() {
+      var queryString = qs.stringify(clone(query), url);
+
+      queryString.should.be.a('string');
+
+      queryString.should.equal(expectedQS);
 
 
     });
   });
 
 });
+
+
 
 function clone(a) {
   return JSON.parse(JSON.stringify(a));
